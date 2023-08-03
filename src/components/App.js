563abe0e609894b3c4bb6346e4b7270cfe073b37
Pranav Badhan <pranavbadhan8@gmail.com>
2023-08-03 08:05:03 +0530

    submitting code-Thu Aug 03 2023 08:05:02 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -2,49 +2,42 @@
 import React, { useState } from "react";
 import './../styles/App.css';
 
-const App = () => {
-  
-  const [todoList, setTodoList] = useState([]);
-
-  const handleAddItems = () => {
-    const newItem = {
-      name: document.getElementById("ItemName").value,
-    };
-
-    const updatedTodoList = [...todoList, newItem];
-
-    setTodoList(updatedTodoList);
-
-    //clear input fields
-
-    document.getElementById('ItemName').value='';
-    
-  }
-
-  const handleRemoveItem = (itemName) => {
-    const updatedTodoList = todoList.filter((item)=>item.name !== itemName);
-    setTodoList(updatedTodoList);
-  }
-
-  return (
-    <div id="main">
-        <div id="add-item">
-          <input type="text" id="ItemName" />
-          <button onClick={handleAddItems}>Add Todo</button>
-        </div>
-
-        {
-          todoList && todoList.map((item, index) => (
-            <div key={index}>
-              <span className="itemName">{item.name}</span>
-              <button onClick={() => handleRemoveItem(item.name)}>Delete</button>
-            </div>
-          ))
+  const App = () =>{
+        
+      let [list,setList] = useState([]);
+      let [item,setItem] = useState("");
+      
+        function addingItem(e){
+            setItem(e.target.value);
         }
 
-    </div>
-  )
+        function addtolist(){
+              setList([...list,item]); 
+              setItem('');      
+        }
 
-}
+        function removeitem(itemName){
+          const updatedTodoList = list.filter((item)=>item !== itemName);
+               setList(updatedTodoList);
+        }
 
-export default App
+    return (
+        <div>
+          <div>
+              <h1>To-Do List</h1>
+              <input type="text" onChange={addingItem} value={item}/>
+              <button onClick={addtolist}>Add Todo</button>
+          </div>
+          {
+            list && list.map((items,index)=>(
+                <div key={index}>
+                    <span className="itemName">{items}</span>
+                     {console.log(items)}
+                    <button onClick={() =>removeitem(items)}>Delete</button>
+                </div>
+              ))
+          }
+        </div>
+    )
+  }
+  export default App;
